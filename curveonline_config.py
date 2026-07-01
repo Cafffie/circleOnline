a52364c7d980d057b469c778ca0772c5be9023be
Arebi Kafayat <143073291+Cafffie@users.noreply.github.com>
2026-07-01 03:51:44 -0700

    Update curveonline_config.py

--- curveonline_config.py
+++ curveonline_config.py
@@ -3,8 +3,6 @@
 SITE_ID = "Curve Online Theatre"
 BASE_URL = "https://www.curveonline.co.uk/"
 RUN_HEADLESS = True
-MAX_RETRIES = 3
-RETRY_DELAY = (2, 4)
 
 PAGES = [
     (f"{BASE_URL}whats-on/?genre-filter=musical", "Musical"),
@@ -17,14 +15,6 @@ COOKIE_BTN_XPATH = (
 
 DEFAULT_CURRENCY = "GBP"
 
-PAGE_LOAD_TIMEOUT = 60
-IFRAME_WAIT_TIMEOUT = 5
-SEAT_WAIT_TIMEOUT = 5
-
-DELAY_BETWEEN_SHOWS = (2, 4)
-DELAY_BETWEEN_PERFS = (1, 3)
-
-
 DEFAULT_THEATRE_DETAILS = {
     "address": "Theatre, Curve, 60 Rutland Street, Leicester, LE1 1SB",
     "venue": "Curve Theatre",
@@ -33,15 +23,15 @@ DEFAULT_THEATRE_DETAILS = {
 }
 
 SELECTORS ={
-    "cookie_button": "//button[@id='CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll']"
+    "cookie_button": "//button[@id='CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll']",
     "theatre_address": ".white-wrapper p.AreaAndVenueDetails",
     "address_header_xpath": "//p[contains(@class, 'h4') and contains(text(), 'Address')]",
     "address_paragraph_xpath": "/following-sibling::p",
     "shows_cards": "article.listing__item",
     "title": "h2.media__title",
-    "terminal_date": "article.listing__item show__date"
+    "terminal_date": "article.listing__item show__date",
     "venue_url": "article.listing__item a",
-    "venue_name": "div.white-wrapper span.AreaName"
+    "venue_name": "div.white-wrapper span.AreaName",
     # Mon 13 - Sat 18 Jul 2026 / Sat 4 Jul 2026
     "year": ".show__time, .show__date",
     "date_blocks": "article.listing__info",
