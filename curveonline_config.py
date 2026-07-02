f1929575d3c156fe8328e70227be8e10bc49cb78
Arebi Kafayat <143073291+Cafffie@users.noreply.github.com>
2026-07-02 17:07:17 +0100

    Update curveonline_config.py

--- curveonline_config.py
+++ curveonline_config.py
@@ -1,8 +1,10 @@
 """Configuration for Curve Online Theatre scraper."""
 
-SITE_ID = "Curve Online Theatre"
+SITE_ID = "curve_online"
 BASE_URL = "https://www.curveonline.co.uk/"
 RUN_HEADLESS = True
+MAX_RETRIES = 3
+RETRY_DELAY = (2, 4)
 
 PAGES = [
     (f"{BASE_URL}whats-on/?genre-filter=musical", "Musical"),
@@ -13,11 +15,19 @@ COOKIE_BTN_XPATH = (
     "//button[@id='CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll']"
 )
 
+HEADLESS = True
+PAGE_LOAD_TIMEOUT = 60
+IFRAME_WAIT_TIMEOUT = 5
+SEAT_WAIT_TIMEOUT = 5
+
+DELAY_BETWEEN_SHOWS = (2, 4)
+DELAY_BETWEEN_PERFS = (1, 3)
+
 DEFAULT_CURRENCY = "GBP"
 
 DEFAULT_THEATRE_DETAILS = {
-    "address": "Theatre, Curve, 60 Rutland Street, Leicester, LE1 1SB",
     "venue": "Curve Theatre",
+    "address": "Theatre, Curve, 60 Rutland Street, Leicester, LE1 1SB",
     "city": "Leicester",
     "country": "UK",
 }
@@ -28,18 +38,15 @@ SELECTORS ={
     "address_header_xpath": "//p[contains(@class, 'h4') and contains(text(), 'Address')]",
     "address_paragraph_xpath": "/following-sibling::p",
     "shows_cards": "article.listing__item",
-    "title": "h2.media__title",
-    "terminal_date": "article.listing__item show__date",
+    "title": "header.flush--right h1.major-title",
+    "terminal_date": "header.flush--right .show__date",
     "venue_url": "article.listing__item a",
-    "venue_name": "div.white-wrapper span.AreaName",
-    # Mon 13 - Sat 18 Jul 2026 / Sat 4 Jul 2026
     "year": ".show__time, .show__date",
     "date_blocks": "article.listing__info",
     "booking_url": "article.listing__info a",
     "raw_date_text": ".listing__date time",
     "raw_time_text": ".listing__time time",
     "iframe": "SpektrixIFrame",
-    "all_seats": "div.SeatingArea img",
+    "seats": "div.SeatingArea img[class*='Seat'], rect.seat",
     "available_seats": "img[class*='Seat']",
-    
 }
